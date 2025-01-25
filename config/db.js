3af48f0eddab6da729c4d4b9d84bcbb8b5fb858f
Jomiloju David <34211095+JomilojuDavid@users.noreply.github.com>
2025-01-25 21:46:34 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -19,21 +19,3 @@ connection.connect((err) => {
 });
 
 module.exports = connection;
-
-// const mysql = require("mysql2");
-// const dotenv = require("dotenv");
-
-// dotenv.config();
-
-// // Create a connection pool
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10, // Maximum number of connections in the pool
-//   queueLimit: 0, // No limit on queued connection requests
-// });
-
-// module.exports = pool;
