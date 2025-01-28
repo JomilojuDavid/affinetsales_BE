002aebf74996a4c193dc446b8a3ccce38644a01f
Jomiloju David <34211095+JomilojuDavid@users.noreply.github.com>
2025-01-28 23:42:55 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,21 +1,23 @@
-require('dotenv').config();
+require('dotenv').config(); 
 
 const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT || 3306 
 };
 
-// Example: Create a MySQL connection
-const mysql = require('mysql');
+const mysql = require('mysql2');
+
 const connection = mysql.createConnection(dbConfig);
 
-connection.connect(err => {
+connection.connect((err) => {
   if (err) {
     console.error('Error connecting to the database:', err.message);
     return;
   }
   console.log('Connected to the database!');
 });
+
+connection.end();
